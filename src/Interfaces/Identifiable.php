ec2b36819b8343246bd3468aae5757f0189fceb1
Nurik <nurik9293709@gmail.com>
2020-09-02 20:54:38 +0600

    fixes

--- src/Interfaces/Identifiable.php
+++ src/Interfaces/Identifiable.php
@@ -5,6 +5,8 @@ namespace BrandStudio\Identifiable\Interfaces;
 interface Identifiable
 {
 
+    public static function identifiableClassName() : string;
+
     public function identifiableName() : string;
 
     public function identifiableLink() : string;
--- src/Traits/Identifiable.php
+++ src/Traits/Identifiable.php
@@ -5,14 +5,29 @@ namespace BrandStudio\Identifiable\Traits;
 trait Identifiable
 {
 
+    public static function identifiableClassName() : string
+    {
+        return trans('admin.'.strtolower(class_basename(static::class)));
+    }
+
     public function identifiableName() : string
     {
-        return trans_choice("admin.{$this->getTable()}", 1)." ".($this->name ?? $this->id);
+        return $this->name ?? $this->id;
     }
 
     public function identifiableLink() : string
     {
         return backpack_url(strtolower(class_basename(static::class)))."/{$this->id}/show";
+    }
+
+    public function getIdentifiableNameAttribute()
+    {
+        return $this->identifiableName();
+    }
+
+    public function getIdentifiableLinkAttribute()
+    {
+        return $this->identifiableLink();
     }
 
 }
